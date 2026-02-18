4193f8e8f7564da6561b6f05bf4992d1ae73992d
gnaneswar2007 <akulagnaneswar19@gmail.com>
2026-02-18 09:43:02 +0530

    18-02-26 springbootmvcdemoapp

---
+++ SpringBootMVCDemoApp/src/main/java/com/klu/SpringBootMVcDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMVcDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMVcDemoAppApplication.class, args);
+	}
+
+}
